4d9ed9b0167607e6dd9889272edcc6a63c6cecab
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2021-03-29 07:26:53 +0100

    scaffolding project

---
+++ confusion.matrix.lib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace confusion.matrix.lib
+{
+    public class Class1
+    {
+    }
+}
---
+++ confusion.matrix.tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace confusion.matrix.tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
